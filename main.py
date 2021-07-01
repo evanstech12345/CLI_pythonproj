352f55b7a676bf2f269e9a3f401e4a7f50606ee7
evanstech12345 <evanshcode@gmail.com>
2021-06-30 21:14:37 -0400

    making the database table

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import click
+import sqlite3
+
+conn = sqlite3.connect('secretfiles.db') 
+
+curs = conn.cursor()
+
+curs.execute("""CREATE TABLE user (
+        Password text,
+        Files BLOB
+        )""")
+
+
+conn.commit()
+conn.close()
+
 print('code is working')
