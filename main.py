a743bb9a2c835ab386dbb467a10020516c474598
evanstech12345 <evanshcode@gmail.com>
2021-07-06 21:31:29 -0400

    finishing the project rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import http.server
 import socketserver
-
+import shutil
 
 
 
@@ -17,21 +17,20 @@ password = input('What is your password\n')
 def login():
     if password == correct_pass:
         open = input('to save the file press S: ')
+    else:
+        print('Wrong password try again.')
     if open == 'S':
-        input('what is the file path: ')
+        file_path = input('what is the file path: ')
+        src = file_path
+        dst = '/opt/homebrew/Caskroom/miniforge/base/envs/CLI_pyproj/CLI_pythonproj/files'
+        shutil.copy(src, dst)
         PORT = 8000
         handler = http.server.SimpleHTTPRequestHandler
         httpd = socketserver.TCPServer(("", PORT), handler)
         print(f"Server at PORT : {PORT}")
         httpd.serve_forever()
-        userinput = input(':')
-    else:
-        print('try again')
-    if userinput == 'S':
-        global path 
-        path = input('what is the file path : ')
-    if path == path:
-        print(f"To access your files go to : {PORT}")
+        
+        
         
 login()
 
